6181e4ab7206b25ae685565fd2ed779d1e70ca59
Michael <primke.michael@gmail.com>
2024-10-14 19:44:24 +0100

    init

--- projects/angular-rsocket/src/public-api.ts
+++ projects/angular-rsocket/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './lib/angular-rsocket.service';
 export * from './lib/angular-rsocket-config';
 export * from './lib/angular-rsocket-tokens';
 export * from './lib/angular-rsocket-provider';
+export * from './lib/angular-rsocket-token-provider';
+export * from './lib/angular-rsocket-token-provider.interface';
